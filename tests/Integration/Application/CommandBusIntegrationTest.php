ec3b18c2070d05110d714089c0a9bd2f17223710
Tomas Prado <tomas@iflnetworks.com>
2023-03-28 17:46:24 +0200

    test(CommandBusIntegrationTest.php): add test for asynchronous command bus

--- tests/Integration/Application/CommandBusIntegrationTest.php
+++ tests/Integration/Application/CommandBusIntegrationTest.php
@@ -6,14 +6,16 @@ declare(strict_types=1);
 
 namespace TeamSquad\Tests\Integration\Application;
 
+use Amp\PHPUnit\AsyncTestCase;
+use Amp\Promise;
 use League\Tactician\CommandBus;
-use PHPUnit\Framework\TestCase;
+use TeamSquad\EventBus\Infrastructure\AsyncSendEventMiddleware;
 use TeamSquad\EventBus\Infrastructure\MemorySecrets;
 use TeamSquad\EventBus\Infrastructure\Rabbit;
 use TeamSquad\EventBus\Infrastructure\SynchronousSendEventMiddleware;
 use TeamSquad\EventBus\SampleRepo\SampleVideoPermissionChangeCommand;
 
-class CommandBusIntegrationTest extends TestCase
+class CommandBusIntegrationTest extends AsyncTestCase
 {
     public function test_synchronous_command_bus(): void
     {
@@ -50,6 +52,46 @@ class CommandBusIntegrationTest extends TestCase
                 'result' => 'OK',
             ],
             json_decode($promise, true, 512, JSON_THROW_ON_ERROR)
+        );
+    }
+
+    public function test_asynchronous_command_bus(): void
+    {
+        $rabbit = Rabbit::getInstance(
+            new MemorySecrets(
+                [
+                    'rabbit_host'  => 'localhost',
+                    'rabbit_port'  => '5672',
+                    'rabbit_user'  => 'guest',
+                    'rabbit_pass'  => 'guest',
+                    'rabbit_vhost' => '/',
+                ]
+            )
+        );
+        $commandBus = new CommandBus(
+            [
+                new AsyncSendEventMiddleware(
+                    'teamsquad.eventBus',
+                    $rabbit
+                ),
+            ]
+        );
+
+        /**
+         * @var Promise<mixed|string> $promise
+         */
+        $promise = $commandBus->handle(
+            new SampleVideoPermissionChangeCommand(
+                '123',
+                false
+            )
+        );
+
+        $value = Promise\wait(
+            Promise\timeout(
+                $promise,
+                20000
+            )
         );
     }
 }
